701c992533d4bacafea0876be1fcf7759a3c6a11
Mark <markgoldstein12@gmail.com>
2019-07-02 13:24:39 -0400

    Added routes for users, auth and contacts. Connected to database

--- config/db.js
+++ config/db.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
-//Mongoose returns promises
+//Mongoose returns promises, let's use async await and try-catch
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
-    });
+    }); //If we get though the await, do this stuff below me
     console.log("MongoDB connected!");
+    //Otherwise catch the  error exit the process.
   } catch (error) {
     console.error(error.message);
     process.exit(1);
