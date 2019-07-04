de6f06590a95a7cd1e3374b65760515faef577f2
Mark <markgoldstein12@gmail.com>
2019-07-03 23:05:33 -0400

    Fixed Auth check and added comments to explain authentication process

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("config");
-// const db = config.get("mongoURI");
-const db = "mongodb://127.0.0.1:27017/music";
-
+const db = config.get("mongoURI");
+// const db = "mongodb://127.0.0.1:27017/music";
 
 //Mongoose returns promises, let's use async await and try-catch
 const connectDB = async () => {
@@ -10,7 +9,7 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false,
+      useFindAndModify: false
     }); //If we get though the await, do this stuff below me
     console.log("MongoDB connected!");
     //Otherwise catch the  error exit the process.
@@ -18,7 +17,7 @@ const connectDB = async () => {
     console.error(err.message + "Went bad here");
     process.exit(1);
   }
-}
+};
 
 // //Use this retry method if you are getting connection failed on first connect.
 // var connectWithRetry = function () {
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const router = express.Router(); //Get express' router
 const User = require("../models/User");
-const {
-  check,
-  validationResult
-} = require("express-validator/check");
+const { check, validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 //Middleware
 const auth = require("../middleware/auth");
-
 
 //POST:Submitting some data/adding contact
 //GET:Fetch/getting data
@@ -26,7 +22,7 @@ const auth = require("../middleware/auth");
 //We EXLUDE the password
 router.get("/", auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password');
+    const user = await User.findById(req.user.id).select("-password");
     res.json(user);
   } catch (err) {
     console.error(err.message);
@@ -40,65 +36,73 @@ router.get("/", auth, async (req, res) => {
 // @desc        Auth user and get Token
 // @access      Public
 // We want to make sure that a email and password is sent
-router.post("/", [
-    check(
-      'email', 'Please include a valid email',
-      check('password', 'Password is required').exists()
-    )
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email"), //Check we got an email
+    check("password", "Password is required").exists() //Check we have a pw
   ],
   async (req, res) => {
+    //This is from express-validator and returns the errors from the checks given the request
     const errors = validationResult(req);
     //If the requirements were NOT met
     if (!errors.isEmpty()) {
+      //Return a status of 400 and an array of the errors
       return res.status(400).json({
         errors: errors.array()
-      }); //Send a error code and an array of the errors
+      });
     }
 
-    const {
-      email,
-      password
-    } = req.body;
+    //If no errors were found(meaning we got a email and pw)
+    const { email, password } = req.body; //Destructure the request's data
 
+    //See if it's valid and see if we can hash it and login
     try {
+      //Use the User model's method findOne to check if the email is actually registered
       let user = await User.findOne({
         email
       });
       if (!user) {
         return res.status(400).json({
-          msg: 'Email not registered!'
-        })
+          msg: "Email not registered!"
+        });
       }
 
+      //If the email exits, let's check the password via a bcrypt.compare hashing
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
         return res.status(400).json({
-          msg: 'Incorrect password!'
-        })
+          msg: "Incorrect password!"
+        });
       }
 
-      //Payload for jwt
+      //If the pw is correct give this payload for jwt
       const payload = {
         user: {
           id: user.id
         }
-      }
+      };
       //jwt takes: Sign, payload, options, and a call back
       //When it expires they'll have to log back in
-      jwt.sign(payload, config.get("jwtSecret"), {
-        expiresIn: 46000
-      }, (err, token) => {
-        if (err) throw err;
-        res.json({
-          token
-        });
-      });
-
+      jwt.sign(
+        //Sign the jwt with the payload given the secret and set it to expire.
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 46000
+        }, //res.json the token as a object
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token
+          });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
     }
-
-  }); //Note that "/" here refers to the prefix of "api/users" + "/"
+  }
+); //Note that "/" here refers to the prefix of "api/users" + "/"
 
 module.exports = router;
