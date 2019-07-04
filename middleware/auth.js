413cad00de32e0f0b2cd83e044105c801bdd2be9
Mark <markgoldstein12@gmail.com>
2019-07-04 12:50:14 -0400

    finished express backend API, allows adding , updating and deleting of contacts. Uses authentication to ensure only the correct(logged in user) can delete/add contacts

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,24 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const config = require("config");
 
 //Middleware is a function that has access to the req/response cycle/object.
-module.exports = function (req, res, next) {
-    //Get the token from the header
-    const token = req.header('x-auth-token');
-    //Check if there is a token
-    if (!token) {
-        return res.status(401).json(({
-            msg: "No token, authorization failed."
-        }));
-    }
-    //If there is a token..
-    try {
-        const decoded = jwt.verify(token, config.get("jwtSecret"));
-        req.user = decoded.user;
-        next(); // move on
-    } catch (err) {
-        res.status(401).json({
-            msg: "Token is invalid!"
-        });
-    }
-}
+module.exports = function(req, res, next) {
+  //Get the token from the header
+  const token = req.header("x-auth-token");
+  //Check if there is a token
+  if (!token) {
+    return res.status(401).json({
+      msg: "No token, authorization failed."
+    });
+  }
+  //If there is a token..
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next(); // move on
+  } catch (err) {
+    res.status(401).json({
+      msg: "Token is invalid!"
+    });
+  }
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -26,7 +26,6 @@ router.get("/", auth, async (req, res) => {
     console.error(err.message);
     res.status("SERVER ERROR");
   }
-  res.send("Gets all contacts");
 }); //Note that "/" here refers to the prefix of "api/users" + "/"
 
 //We can have the same request paths for two different methods!
@@ -75,16 +74,60 @@ router.post(
 // @ route      PUT api/contacts
 // @desc        Update a contact
 // @access      Private
-router.put("/:id", (req, res) => {
-  res.send("Added a contact"); // the ":id" is a placeholder
+router.put("/:id", auth, async (req, res) => {
+  // the ":id" is a placeholder
+  //Pull out the data
+  const { name, email, phone, type } = req.body;
+
+  //Build a contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+  console.log("Im here");
+  try {
+    //Find the contact by id
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+    //Make sure user owns this contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "You're not authorized for this." });
+    }
+    //Do the update
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+    res.json({ contact });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("SERVER ERROR");
+  }
 }); //Note that "/" here refers to the prefix of "api/contacts" + "/"
 
 //We can have the same request paths for two different methods!
 // @ route      DELETE api/contacts
 // @desc        Delete a contact
 // @access      Private
-router.delete("/:id", (req, res) => {
-  res.send("Deleted a contact"); // the ":id" is a placeholder
+router.delete("/:id", auth, async (req, res) => {
+  console.log("Im here");
+  try {
+    //Find the contact by id
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+    //Make sure user owns this contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "You're not authorized for this." });
+    }
+    //Delete the contact
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Contact Removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("SERVER ERROR");
+  }
 }); //Note that "/" here refers to the prefix of "api/contacts" + "/"
 
 module.exports = router;
