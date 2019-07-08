37d2f9e99a263b59cd4eee63819b301335bdd38c
Mark <markgoldstein12@gmail.com>
2019-07-08 18:07:36 -0400

    'Test'

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
+const db1 = config.get("mongoURI1");
 // const db = "mongodb://127.0.0.1:27017/music";
 
 //Mongoose returns promises, let's use async await and try-catch
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(db || db1, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
