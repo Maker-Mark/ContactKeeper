15663ad9cbc932ad9f45342744324a60e9f6c862
Mark <markgoldstein12@gmail.com>
2019-07-08 17:35:58 -0400

    test

--- server.js
+++ server.js
@@ -2,27 +2,58 @@ const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
 
-//Setting up the express server
 const app = express();
 
-//Connect the database
+// Connect Database
 connectDB();
 
-//Init middleware
-app.use(express.json({ extended: false })); //Now we can accept body data
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-//Define Routes
-app.use("/api/users", require("./routes/users")); //All backend routes start with "/api"
+// Define Routes
+app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-//Serve static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  //Set static folder
+  // Set static folder
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Express server running on ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// const express = require("express");
+// const connectDB = require("./config/db");
+// const path = require("path");
+
+// //Setting up the express server
+// const app = express();
+
+// //Connect the database
+// connectDB();
+
+// //Init middleware
+// app.use(express.json({ extended: false })); //Now we can accept body data
+
+// //Define Routes
+// app.use("/api/users", require("./routes/users")); //All backend routes start with "/api"
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/contacts", require("./routes/contacts"));
+
+// //Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   //Set static folder
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Express server running on ${PORT}`));
