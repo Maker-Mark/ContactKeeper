b2da6ecb1e29fefe99d28774cfb0f567abea52c4
Mark <markgoldstein12@gmail.com>
2019-07-08 17:47:06 -0400

    'test'

--- config/db.js
+++ config/db.js
@@ -4,30 +4,30 @@ const db = config.get("mongoURI");
 // const db = "mongodb://127.0.0.1:27017/music";
 
 //Mongoose returns promises, let's use async await and try-catch
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(db, {
-//       useNewUrlParser: true,
-//       useCreateIndex: true,
-//       useFindAndModify: false
-//     }); //If we get though the await, do this stuff below me
-//     console.log("MongoDB connected!");
-//     //Otherwise catch the  error exit the process.
-//   } catch (err) {
-//     console.error(err.message + "Went bad here");
-//     process.exit(1);
-//   }
-// };
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
 
-//Use this retry method if you are getting connection failed on first connect.
-var connectWithRetry = function() {
-  return mongoose.connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message + "Went bad here");
+    process.exit(1);
+  }
 };
-connectDB = connectWithRetry;
+
+// //Use this retry method if you are getting connection failed on first connect.
+// var connectWithRetry = function() {
+//   return mongoose.connect(db, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   });
+// };
+// connectDB = connectWithRetry;
 
 // //Until here
 
